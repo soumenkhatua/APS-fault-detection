8c47a55470457c16620a8288818c5610f3ebb94e
soumenkhatua <soumenkhatua60@gmail.com>
2022-12-01 19:29:05 +0530

    Updated the requirements

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages,setup
+
+
+def get_requirements():
+    pass
+
+
+
+
+setup(
+    name="sensor",
+    version = "0.0.1",
+    author ="Soumen",
+    author_email="soumenkhatua60@gmail.com",
+    packages=find_packages(),
+    install_requirements=get_requirements(),
+)
